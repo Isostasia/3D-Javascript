a5502135996e436f1311e4f146a429a1b0e6cc50
EJ <etovarda@my.centennialcollege.ca>
2023-04-08 14:25:51 -0400

    Setting up App Environment
    Modules:
    *TailwindCSS
    *Autoprefixer
    *PostCSS
    *Svelte Pre-processor
             *SASS
    *Three.js
    
    Plugins
    *Tailwind forms styling

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+</script>
+  
+<div id="user-settings" class="flex flex-column flex-wrap h-screen max-w-xs bg-red-500 justify-center">
+    <div id="shape-size" class="w-full ">
+        Hola
+    </div>
+    <div id="shape-color" class="w-full">
+        Hola
+    </div>
+    <div id="shape-rotation" class="w-full">
+        Hola
+    </div>
+</div>
+
+<style lang="postcss">
+    /*mobile*/
+    :global(html) {
+        background-color: theme(colors.sky.100);
+    }
+    /*variables*/
+    :global(root){
+
+    }
+
+</style>
